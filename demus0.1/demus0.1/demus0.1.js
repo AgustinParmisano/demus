8add52df13f4411616fa85f26fdd2db19fabdd66
pantufla <pantufla@thermaltake>
2014-12-07 00:16:32 -0300

    Logrando que se pueda votar de a poco

--- demus0.1/demus0.1/demus0.1.js
+++ demus0.1/demus0.1/demus0.1.js
@@ -1,19 +1,36 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+Videos = new Mongo.Collection("videos");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+if (Meteor.isClient) {
+
+  Template.videos.helpers({
+    videosToVote: function () {
+      return Videos.find({estado:"paraVotar"});
     }
   });
 
-  Template.hello.events({
+  Template.videosToVoteButtons.helpers({
+    videosToVote: function () {
+      return Videos.find({estado:"paraVotar"});
+    }
+  });
+
+  Template.videos.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
     }
   });
+
+  Template.videosToVoteButtons.events({
+    "click .votingButtons": function (event) {
+      var votedVideo = event.target.value;
+      console.log(votedVideo);
+      var videoID = Videos.findOne({name: votedVideo});
+      console.log(videoID);
+      Videos.update({_id: videoID._id}, {$inc: {votesToday: 1, votesHistory: 1}});
+    } 
+  });
+
+
 }
 
 if (Meteor.isServer) {
